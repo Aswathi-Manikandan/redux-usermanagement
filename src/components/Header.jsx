9404be1ee815c6d06748b6241ca6fc3e4b7f5bc1
ahwathy <aswathiachus935@gmail.com>
2024-11-18 17:46:28 +0530

    create node server

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,18 @@ const Header = () => {
   return (
     <div className='bg-slate-600'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+            <Link to='/'>
             <h1 className='font-bold'>Usermanagement</h1>
+            </Link>
+
             <ul className='flex gap-4'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Sign In</li>
+
+                <Link to='/'> <li>Home</li></Link>
+               
+                <Link to='/about'> <li>About</li></Link>
+
+                <Link to='/sign-in'> <li>Sign In</li></Link>
+
             </ul>
         </div>
       
